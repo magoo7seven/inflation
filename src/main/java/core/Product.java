21cee8ae92c936048db6414359abbb6c66180513
Marcin <grzegorczykmarcin4@gmail.com>
2023-06-23 19:28:12 +0200

    [PLANET - 0001] Set up new project, added configuration for GitHub

---
+++ src/main/java/core/Product.java
@@ -0,0 +1,46 @@
+package core;
+
+/**
+ * Base class for each product used in application
+ */
+public class Product {
+
+    /**
+     * Name of each product
+     */
+    private String productName;
+
+    /**
+     * Price of the product in polish zł
+     */
+    private int price;
+
+    public Product(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
